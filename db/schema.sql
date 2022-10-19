28453d86843d0538f5f136421bcacbe20f4c2313
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-10-19 05:26:18 -0500

    query time

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- DEPARTMENT TABLE ----
-CREATE TABLE departments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) 
 );
 
--- EMPLOYEE ROLE TABLE ----
-CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
+-- ROLE TABLE ----
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES departments(id)
+    -- FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 -- EMPLOYEE ROLE TABLE ----
-CREATE TABLE employees (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
+    -- FOREIGN KEY (role_id) REFERENCES roles(id),
+    -- FOREIGN KEY (manager_id) REFERENCES employeesSELECT * FROM department_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,7 +9,7 @@ VALUES  ('Management'),
 INSERT INTO roles (title, salary, department_id)
 VALUES  ('Account manager', 58584, 1),
         ('Stylist', 33463, 4),
-        ('Fashion designer', 42017, 2)
+        ('Fashion designer', 42017, 2),
         ('Quality assurance manager', 69442, 3),
         ('Sales associate', 31139,4),
         ('Inventory planner', 57012, 1),
@@ -22,9 +22,8 @@ VALUES  ('Cindy', 'Miller',     1,  null),
         ('Xaviar', 'Sanders',   2,  null),
         ('Tom', 'Holland',      3,  null),
         ('Stephanie', 'Soo',    4,  null),
-        ('Lawrance','Crawford', 5,  4),
+        ('Lawrance', 'Crawford', 5,  4),
         ('Pluto', 'Starlight',  6,  5),
         ('Celine', 'Takahashi', 7,  2),
         ('Rebekah', 'Kim',      8,  3);
 
-UPDATE `employee_db`.`employees` SET `manager_id` = '1' WHERE (`id` > '1');
