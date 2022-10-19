906ca0d9f7f2fb7c3303947cb16e37bb0e8a9584
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-10-19 03:00:06 -0500

    seeds complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,25 @@
-INSERT INTO book_prices (price)
-VALUES (1),
-       (2),
-       (5),
-       (10),
-       (15);
+INSERT INTO department (division)
+VALUES  ('Management'),
+        ('The Fashion Design Department'),
+        ('Product Development'),
+        ('Styling');
 
-INSERT INTO favorite_books (book_name, in_stock, book_price)
-VALUES ("The Great Gatsby", true, 1),
-       ("Huckleberry Finn", true, 3),
-       ("100 Years of Solitude", false, 5),
-       ("Things Fall Apart", false, 1),
-       ("Crime and Punishment", true, 2),
-       ("Moby Dick", true, 4),
-       ("Decameron", false, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES  ('Account manager', 58584, 1),
+        ('Stylist', 33463, 4),
+        ('Fashion designer', 42017, 2)
+        ('Quality assurance manager', 69442, 3),
+        ('Sales associate', 31139,4),
+        ('Inventory planner', 57012, 1),
+        ('Creative director', 77774, 2),
+        ('Technical designer', 63874, 3);
 
-INSERT INTO book_prices (price)
-VALUES (1),
-       (2),
-       (5),
-       (10),
-       (15);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Cindy', 'Miller', 1, null),
+        ('Xaviar', 'Sanders', 2, 1),
+        ('Tom', 'Holland', 3, null),
+        ('Stephanie', 'Soo', 4, 3),
+        ('Lawrance', 'Crawford', 5, null),
+        ('Pluto', 'Starlight', 6, 5),
+        ('Celine', 'Takahashi', 7, null),
+        ('Rebekah', 'Kim', 8, 7);
