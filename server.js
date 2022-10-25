d43e3fcde480aab7b08a57d53124438c0be0071a
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-10-25 18:21:09 -0500

    start app

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const startScreen = ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
 const allEmployeeQuery = `SELECT e.id, e.first_name AS "First Name", e.last_name AS "Last Name", r.title, d.department_name AS "Department", IFNULL(r.salary, 'No Data') AS "Salary", CONCAT(m.first_name," ",m.last_name) AS "Manager"
-FROM employees e
-LEFT JOIN roles r 
-ON r.id = e.role_id 
-LEFT JOIN departments d 
-ON d.id = r.department_id
-LEFT JOIN employees m ON m.id = e.manager_id
-ORDER BY e.id;`
+      FROM employees e
+      LEFT JOIN roles r 
+      ON r.id = e.role_id 
+      LEFT JOIN departments d 
+      ON d.id = r.department_id
+      LEFT JOIN employees m ON m.id = e.manager_id
+      ORDER BY e.id;`
 
 const addQuestions = ['What is the first name?', 'What is the last name?', 'What is their role?', 'Who is their manager?']
 const roleQuery = 'SELECT * from roles; SELECT CONCAT (e.first_name," ",e.last_name) AS full_name FROM employees e'
@@ -62,8 +62,8 @@ const showAll = () => {
         console.table('All Employees'), results
         startApp();
     })
-
 }
+
 
 startApp();
 
